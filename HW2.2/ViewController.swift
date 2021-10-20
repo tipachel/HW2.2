4c128244cf74352952521a030e4fd3bbe68f18d6
Boiko Konstantin <tipakoroche@gmail.com>
2021-10-20 21:13:31 +0300

    задание 2.0

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = 65
+        yellowLight.layer.cornerRadius = 65
+        greenLight.layer.cornerRadius = 65
+        
+        
     }
-
-
+    
+    @IBAction func changeButtonPressed() {
+        
+        changeButton.setTitle("NEXT", for: .normal)
+        if redLight.alpha < 1 && yellowLight.alpha < 1{
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+        }else if redLight.alpha == 1{
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        }else if yellowLight.alpha == 1{
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+       
+        }
+        
+        
+    }
 }
 
